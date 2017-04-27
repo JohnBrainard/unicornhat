81bcd42e362c71623f0ce7e2faee6314b02b1d22
John <john@localhost.localdomain>
2017-04-27 06:44:51 -0700

    Add RGB/GRB mode options, housecleaning, error handling

--- color.go
+++ color.go
@@ -14,7 +14,11 @@ func ColorNew(red byte, green byte, blue byte) Color {
 	return Color{Red: red, Green: green, Blue: blue}
 }
 
-func (c *Color) Bytes() ([]byte) {
+func (c *Color) Bytes(mode byte) ([]byte) {
+	if mode == MODE_RGB {
+		return []byte{c.Red, c.Green, c.Blue}
+	}
+
 	return []byte{c.Green, c.Red, c.Blue}
 }
 
--- unicorn.go
+++ unicorn.go
@@ -5,66 +5,91 @@ import (
 	"bytes"
 )
 
-const SocketPath = "/var/run/unicornd.socket"
+const (
+	SocketPath = "/var/run/unicornd.socket"
 
-const CMD_SET_BRIGHTNESS byte = 0
-const CMD_SET_PIXEL byte = 1
-const CMD_SET_ALL_PIXELS byte = 2
-const CMD_SHOW byte = 3
+	MODE_RGB byte = 0
+	MODE_GRB byte = 1
 
-type UnicornHat struct {
+	CMD_SET_BRIGHTNESS byte = 0
+	CMD_SET_PIXEL      byte = 1
+	CMD_SET_ALL_PIXELS byte = 2
+	CMD_SHOW           byte = 3
+)
+
+type Hat struct {
 	socket net.Conn
+	mode   byte
 }
 
-func Connect() (*UnicornHat, error) {
+func Connect() (*Hat, error) {
 	return ConnectToSocket(SocketPath)
 }
 
-func ConnectToSocket(path string) (*UnicornHat, error) {
+func ConnectToSocket(path string) (*Hat, error) {
 	socket, err := net.Dial("unix", SocketPath)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &UnicornHat{socket: socket}, nil
+	return &Hat{socket: socket, mode: MODE_GRB}, nil
 }
 
-func (uh *UnicornHat) SetBrightness(brightness byte) {
+func (uh *Hat) SetRGBMode() {
+	uh.mode = MODE_RGB
+}
+
+func (uh *Hat) SetGRBMode() {
+	uh.mode = MODE_GRB
+}
+
+func (uh *Hat) SetBrightness(brightness byte) error {
 	buf := new(bytes.Buffer)
 
 	buf.WriteByte(CMD_SET_BRIGHTNESS)
 	buf.WriteByte(brightness)
 
-	uh.socket.Write(buf.Bytes())
+	_, err := uh.socket.Write(buf.Bytes())
+	return err
 }
 
-func (uh *UnicornHat) Show() {
-	uh.socket.Write([]byte{CMD_SHOW})
+func (uh *Hat) Show() error {
+	_, err := uh.socket.Write([]byte{CMD_SHOW})
+	return err
 }
 
-func (uh *UnicornHat) SetPixel(x byte, y byte, color Color) {
+func (uh *Hat) SetPixel(x byte, y byte, color Color) error {
 	buf := new(bytes.Buffer)
 
 	buf.Write([]byte{CMD_SET_PIXEL})
 	buf.Write([]byte{x, y})
-	buf.Write(color.Bytes())
+	buf.Write(color.Bytes(uh.mode))
 
-	uh.socket.Write(buf.Bytes())
+	_, err := uh.socket.Write(buf.Bytes())
+	return err
 }
 
-func (uh *UnicornHat) SetAllPixels(pixels [64]Color) {
+func (uh *Hat) SetAllPixels(pixels [64]Color) error {
 	buf := new(bytes.Buffer)
 
 	buf.WriteByte(CMD_SET_ALL_PIXELS)
 
 	for _, color := range pixels {
-		buf.Write(color.Bytes())
+		buf.Write(color.Bytes(uh.mode))
 	}
 
-	uh.socket.Write(buf.Bytes())
+	_, err := uh.socket.Write(buf.Bytes())
+	return err
 }
 
-func (uh *UnicornHat) Clear() {
-	uh.SetAllPixels([64]Color{})
+func (uh *Hat) Clear() error {
+	return uh.SetAllPixels([64]Color{})
+}
+
+func (uh *Hat) Close() error {
+	err := uh.socket.Close()
+	uh.socket = nil
+
+	return err
 }
